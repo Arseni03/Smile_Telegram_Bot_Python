36412b1bb3b20ba45d419642deaf24b257a611a4
Arseni_Rusin <Rusin.senya@mail.ru>
2020-06-18 15:31:10 +0300

    last commit

---
+++ venv/bot_stuff.py
@@ -0,0 +1,19 @@
+Smails= [':smiling_cat_face_with_heart-eyes:',':flushed_face:',':anguished_face:',':face_with_steam_from_nose:', #0-3
+':neutral_face:',':smirking_face:',':fearful_face:',':downcast_face_with_sweat:', #4-7
+':smiling_face_with_smiling_eyes:',':smiling_face_with_horns:',':slightly_smiling_face:',':expressionless_face:',':hugging_face:', #8-12
+':kissing_face:',':smiling_face_with_heart-eyes:',':smiling_face_with_3_hearts:',':face_blowing_a_kiss:',':lying_face:', #13-17
+':grinning_cat_face:',':kissing_face_with_closed_eyes:',':relieved_face:',':tired_face:',':yawning_face:', #18-22
+':drooling_face:',':zany_face:',':face_savoring_food:',':grinning_face:',':grinning_face_with_sweat:',':face_with_tears_of_joy:', #23-28
+':upside-down_face:',':face_with_monocle:',':nerd_face:',':unamused_face:',':smiling_face_with_sunglasses:',':star-struck:',':face_with_tongue:',':confused_face:', #29-36
+':slightly_frowning_face:',':persevering_face:',':confounded_face:',':face_without_mouth:',':weary_face:',':pleading_face:',':crying_face:', #37-43
+':loudly_crying_face:',':face_with_medical_mask:',':alien:',':pouting_face:',':grinning_squinting_face:',':winking_face_with_tongue:'] #44-49
+
+Smails_Text = ['^ↀᴥↀ^', '(ಠ_ಠ)', 'ಠoಠ', '( ͡°෴ ͡°)', #0-3
+'◉_◉', '(¬‿¬)', '(ʘᗩʘ)', '(；一_一)', #4-7
+'( ͡ᵔ ͜ʖ ͡ᵔ )', '(◣∀◢)ψ', '◉◡◉', '(￣x￣;)', '＼(￣▽￣)／', #8-12
+'( ́ ◕◞ε◟◕`)', '(｡♥️‿♥️｡)', '(◕‿◕)♡', '( ･_･)♡', '( ͡° ͜ ͜つ ͡° )', #13-17
+'-^o^-', '(╯3╰)', '(⌒_⌒)', '(＞﹏＜)', '(￣O￣)ツ', #18-22
+'(￣﹃￣)', '|◔◡◉|', '(っ˘ڡ˘ς)', '( ° ͜ʖ °', '( ͡° ͜ʖ ͡°; )', '(TᗜT)', #23-28
+'( ͜。 ͡ʖ ͜。)', '( ͠°ヘ °)', '(ꝊᨓꝊ)', '〳 ͡° Ĺ̯ ͡° 〵', '(■◡■)', '(⨱◡⨱)', '(ⱺ∀ⱺ)', '(￣～￣)', #29-36
+'⊙︿⊙', '＼(＾∀＾)メ', '(>_<)', '(>_<)', '(◢ д ◣)', '(ಥ﹏ಥ)', '(ಥ_ಥ)', #37-43
+'(╥_╥)', '(-ロ-)', '⎝❍◡❍⎠', '୧( ಠ Д ಠ )୨', '(ᗒᗜᗕ)', '(⪰∀ⱺ)'] #44-49
